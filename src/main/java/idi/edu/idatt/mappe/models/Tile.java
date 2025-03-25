e2f8d0ef97bbcafbad912880047b51509326b369
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-03-25 17:22:30 +0100

    Update Tile.java
    Updated Tile to also have Coordinates

--- src/main/java/idi/edu/idatt/mappe/models/Tile.java
+++ src/main/java/idi/edu/idatt/mappe/models/Tile.java
@@ -7,13 +7,13 @@ import static idi.edu.idatt.mappe.validators.TileValidator.validateTileAction;
 
 /**
  * Represents a tile on the board
- *
- *
  */
 public class Tile {
     private Tile nextTile;
     private int tileId;
     private TileAction landAction;
+    private int x; // x-coordinate on the board
+    private int y; // y-coordinate on the board
 
     /**
      * Creates a new tile with the given id
@@ -25,9 +25,22 @@ public class Tile {
     }
 
     /**
+     * Creates a new tile with the given id and coordinates
+     *
+     * @param tileId The id of the tile
+     * @param x The x-coordinate of the tile
+     * @param y The y-coordinate of the tile
+     */
+    public Tile(int tileId, int x, int y) {
+        this.tileId = tileId;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
      * Sets the action to be performed when a player lands on this tile
      *
-     * @param player
+     * @param player The player who landed on the tile
      */
     public void landPlayer(Player player) {
         validatePlayer(player);
@@ -41,7 +54,7 @@ public class Tile {
     /**
      * Sets the action to be performed when a player leaves this tile
      *
-     * @param player
+     * @param player The player who is leaving the tile
      */
     public void leavePlayer(Player player) {
         // TODO: Implement
@@ -57,9 +70,9 @@ public class Tile {
     }
 
     /**
-     * Returns the next tile
+     * Sets the next tile
      *
-     * @return The next tile
+     * @param nextTile The next tile
      */
     public void setNextTile(Tile nextTile) {
         this.nextTile = nextTile;
@@ -96,11 +109,58 @@ public class Tile {
     }
 
     /**
-     *  Returns the index of the tile
+     * Returns the index of the tile
      *
      * @return The index of the tile
      */
     public int getIndex() {
         return tileId;
+    }
+
+    /**
+     * Returns the x-coordinate of the tile
+     *
+     * @return The x-coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Sets the x-coordinate of the tile
+     *
+     * @param x The x-coordinate
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Returns the y-coordinate of the tile
+     *
+     * @return The y-coordinate
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets the y-coordinate of the tile
+     *
+     * @param y The y-coordinate
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Sets both x and y coordinates of the tile
+     *
+     * @param x The x-coordinate
+     * @param y The y-coordinate
+     */
+    public void setCoordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
