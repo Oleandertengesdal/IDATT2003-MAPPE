37de5080a2aa97335d2893870ba25ec2d2595de1
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-03-25 17:21:50 +0100

    Create ConfigFileReader.java

---
+++ src/main/java/idi/edu/idatt/mappe/utils/ConfigFileReader.java
@@ -0,0 +1,27 @@
+package idi.edu.idatt.mappe.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class ConfigFileReader {
+
+    private ConfigFileReader() {
+        // Private constructor to hide the implicit public one
+    }
+
+    public static Logger getLogger(Class<?> clazz) {
+        try (InputStream configFile = clazz.getClassLoader().getResourceAsStream("logging.properties")) {
+            if (configFile != null) {
+                LogManager.getLogManager().readConfiguration(configFile);
+            } else {
+                Logger.getLogger(clazz.getName()).log(Level.SEVERE, "Could not load logging.properties file");
+            }
+        } catch (IOException e) {
+            Logger.getLogger(clazz.getName()).log(Level.SEVERE, "Error loading logging configuration", e);
+        }
+        return Logger.getLogger(clazz.getName());
+    }
+}
