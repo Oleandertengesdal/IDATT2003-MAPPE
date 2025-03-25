bb4837653f6c0f648b39437cb7f5a78ec3bc14f9
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-03-25 17:21:35 +0100

    Create PlayerFileWriter.java

---
+++ src/main/java/idi/edu/idatt/mappe/files/writer/PlayerFileWriter.java
@@ -0,0 +1,19 @@
+package idi.edu.idatt.mappe.files.writer;
+
+import idi.edu.idatt.mappe.models.Player;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class PlayerFileWriter {
+    public static void savePlayersToCSV(List<Player> players, String filePath) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (Player player : players) {
+                writer.write(player.getName() + "," + player.getToken() + "\n");
+            }
+        } catch (IOException e) {
+            throw new IOException("Could not write to file");
+        }
+    }
+}
