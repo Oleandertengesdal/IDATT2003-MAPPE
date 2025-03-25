bedc9c49667db9f17ed40867135cb2fb015818cf
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-03-25 19:15:10 +0100

    Update BoardFileWriter.java

--- src/main/java/idi/edu/idatt/mappe/files/writer/BoardFileWriter.java
+++ src/main/java/idi/edu/idatt/mappe/files/writer/BoardFileWriter.java
@@ -1,5 +1,6 @@
 package idi.edu.idatt.mappe.files.writer;
 
+import com.google.gson.JsonParseException;
 import idi.edu.idatt.mappe.exceptions.JsonParsingException;
 import idi.edu.idatt.mappe.exceptions.TileActionNotFoundException;
 import idi.edu.idatt.mappe.models.Board;
@@ -8,4 +9,6 @@ import java.io.IOException;
 
 public interface BoardFileWriter {
     public void writeBoard(Board board, String filePath) throws JsonParsingException, IOException, TileActionNotFoundException;
+
+    public void writeBoard(Board board, String fileName, String name, String description) throws JsonParseException, IOException, TileActionNotFoundException;
 }
