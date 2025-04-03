df0429c1ff9691043072141afc088caaf1bc6ac8
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-04-03 18:24:38 +0200

    Creating view

--- src/main/java/idi/edu/idatt/mappe/files/reader/PlayerFileReader.java
+++ src/main/java/idi/edu/idatt/mappe/files/reader/PlayerFileReader.java
@@ -8,20 +8,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerFileReader {
-    public static List<Player> loadPlayersFromCSV(String filePath) throws IOException {
-        List<Player> players = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    players.add(new Player(parts[0], parts[1]));
-                }
-            }
-        } catch (IOException e) {
-            throw new IOException("Could not read file");
-        }
-        return players;
-    }
+/**
+ * PlayerFileReader is an interface for reading player data from a file.
+ * It provides a method to load a list of players from a specified file path.
+ */
+public interface PlayerFileReader {
+    public List<Player> loadPlayers(String filePath) throws IOException;
 }
--- src/main/java/idi/edu/idatt/mappe/files/writer/PlayerFileWriter.java
+++ src/main/java/idi/edu/idatt/mappe/files/writer/PlayerFileWriter.java
@@ -6,14 +6,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-public class PlayerFileWriter {
-    public static void savePlayersToCSV(List<Player> players, String filePath) throws IOException {
-        try (FileWriter writer = new FileWriter(filePath)) {
-            for (Player player : players) {
-                writer.write(player.getName() + "," + player.getToken() + "\n");
-            }
-        } catch (IOException e) {
-            throw new IOException("Could not write to file");
-        }
-    }
+/**
+ * PlayerFileWriter is an interface for writing player data to a file.
+ * It provides a method to save a list of players to a specified file path.
+ */
+public interface PlayerFileWriter {
+    public void savePlayers(List<Player> players, String filePath) throws IOException;
 }
