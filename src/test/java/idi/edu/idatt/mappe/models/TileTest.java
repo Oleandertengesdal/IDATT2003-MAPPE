a46194033b9028b5eb06b3c5d5adbb3fafbfa65e
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-05-23 00:24:28 +0200

    Update TileTest.java

--- src/test/java/idi/edu/idatt/mappe/models/TileTest.java
+++ src/test/java/idi/edu/idatt/mappe/models/TileTest.java
@@ -1,5 +1,7 @@
 package idi.edu.idatt.mappe.models;
 
+import idi.edu.idatt.mappe.models.enums.Direction;
+import idi.edu.idatt.mappe.models.enums.TileType;
 import idi.edu.idatt.mappe.models.tileaction.LadderTileAction;
 import idi.edu.idatt.mappe.models.tileaction.TileAction;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,64 +12,73 @@ import static org.junit.jupiter.api.Assertions.*;
 class TileTest {
 
     private Tile tile;
-    private Tile nextTile;
-    private TileAction action;
+    private Tile connectedTile;
     private Player player;
-    private BoardGame board;
+    private TileAction action;
 
     @BeforeEach
     void setUp() {
-        tile = new Tile(1);
-        nextTile = new Tile(2);
+        tile = new Tile(1, "Test City", TileType.CITY, 0, 0);
+        connectedTile = new Tile(2, "Connected City", TileType.CITY, 1, 1);
         player = new Player("TestPlayer");
-        board = new BoardGame();
-        board.createBoard(100);
+        action = new LadderTileAction(2, "Climb the ladder", null);
     }
 
     @Test
     void testSetAndGetNextTile() {
-        tile.setNextTile(nextTile);
-        assertEquals(nextTile, tile.getNextTile());
+        tile.setNextTile(connectedTile);
+        assertEquals(connectedTile, tile.getNextTile());
     }
 
     @Test
-    void testSetAndPerformLandAction() {
-        action = new TileAction() {
-            @Override
-            public void perform(Player player) {
-                player.setExtraThrow(true);
-            }
-        };
+    void testSetAndGetLandAction() {
         tile.setLandAction(action);
-        tile.performLandAction(player);
-        assertTrue(player.hasExtraThrow());
+        assertEquals(action, tile.getLandAction());
     }
 
     @Test
-    void testGetIndex() {
-        assertEquals(1, tile.getIndex());
+    void testAddAndGetConnections() {
+        tile.addConnection(Direction.NORTH, connectedTile, 10);
+        assertEquals(connectedTile, tile.getConnectionInDirection(Direction.NORTH));
+        assertEquals(10, tile.getTravelCost(Direction.NORTH));
     }
 
     @Test
-    void testLandPlayer() {
-        action = new TileAction() {
-            @Override
-            public void perform(Player player) {
-                player.setExtraThrow(true);
-            }
-        };
-        tile.setLandAction(action);
-        tile.landPlayer(player);
-        assertTrue(player.hasExtraThrow());
+    void testHasToken() {
+        assertFalse(tile.hasToken());
+        tile.setHiddenToken(null);
+        assertFalse(tile.hasToken());
     }
 
     @Test
-    void testLadderTileAction() {
-        Tile destinationTile = new Tile(3);
-        board.getBoard().getTiles().put(3, destinationTile);
-        LadderTileAction ladderAction = new LadderTileAction(3, "Climb the ladder", board.getBoard());
-        tile.setLandAction(ladderAction);
-        tile.performLandAction(player);
-        assertEquals(destinationTile, player.getCurrentTile());
+    void testRevealToken() {
+        tile.setHiddenToken(null);
+        assertNull(tile.revealToken(player));
+    }
+
+    @Test
+    void testGetAffordableConnections() {
+        tile.addConnection(Direction.NORTH, connectedTile, 10);
+        player.setMoney(20);
+        assertTrue(tile.getAffordableConnections(player).contains(connectedTile));
+    }
+
+    @Test
+    void testSetAndGetCoordinates() {
+        tile.setCoordinates(5, 10);
+        assertEquals(5, tile.getX());
+        assertEquals(10, tile.getY());
+    }
+
+    @Test
+    void testIsGoal() {
+        tile.setGoal(true);
+        assertTrue(tile.isGoal());
+    }
+
+    @Test
+    void testIsStart() {
+        tile.setStart(true);
+        assertTrue(tile.isStart());
     }
 }
