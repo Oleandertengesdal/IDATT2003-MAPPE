aae86cadc0db5b2b225c1bd0eda77a6e6b5b011a
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-05-23 00:22:12 +0200

    Update BoardGameController.java
    Added players, and removed unused methods.

--- src/main/java/idi/edu/idatt/mappe/controllers/BoardGameController.java
+++ src/main/java/idi/edu/idatt/mappe/controllers/BoardGameController.java
@@ -1,20 +1,14 @@
 package idi.edu.idatt.mappe.controllers;
 
-import idi.edu.idatt.mappe.exceptions.JsonParsingException;
-import idi.edu.idatt.mappe.exceptions.TileActionNotFoundException;
-import idi.edu.idatt.mappe.models.Board;
 import idi.edu.idatt.mappe.models.BoardGame;
 import idi.edu.idatt.mappe.models.Player;
 import idi.edu.idatt.mappe.models.PlayerSelectionEntry;
 import idi.edu.idatt.mappe.models.enums.GameState;
-import idi.edu.idatt.mappe.controllers.FileService;
 import idi.edu.idatt.mappe.views.GameView;
 
-import java.io.File;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -88,6 +82,15 @@ public abstract class BoardGameController {
     }
 
     /**
+     * gets the players in the game.
+     *
+     * @return A list of players in the game
+     */
+    public List<Player> getPlayers() {
+        return boardGame.getPlayers();
+    }
+
+    /**
      * Adds a player to the game with the given name and token.
      *
      * @param playerEntry The player entry containing player and color information
@@ -101,8 +104,8 @@ public abstract class BoardGameController {
                 " color: " + colorHex);
 
         gameView.getColorService().setPlayerColorFromHex(player, colorHex);
-        boardGame.addPlayer(player);
-        gameView.addPlayer(player);
+
+        this.addPlayer(player);
 
         logger.info("Player fully added with color: " + colorHex);
     }
@@ -125,6 +128,8 @@ public abstract class BoardGameController {
 
         gameView.logGameEvent("Game started with " + boardGame.getPlayers().size() + " players. " +
                 "Current player: " + firstPlayerName);
+
+        gameView.setRollDiceButtonEnabled(true);
     }
 
     /**
@@ -150,6 +155,8 @@ public abstract class BoardGameController {
         List<Integer> values = boardGame.getDice().getValues();
         logger.info("Dice values: " + values);
 
+        gameView.updateDiceDisplay(values);
+
         gameView.logGameEvent("Dice rolled: " + values.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(", ")) + " (Total: " + total + ")");
@@ -166,7 +173,11 @@ public abstract class BoardGameController {
         currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
 
         Player nextPlayer = players.get(currentPlayerIndex);
+        boardGame.setCurrentPlayer(nextPlayer);
+
         gameView.logGameEvent("Next turn: " + nextPlayer.getName());
+
+        gameView.setRollDiceButtonEnabled(true);
     }
 
     /**
@@ -196,136 +207,6 @@ public abstract class BoardGameController {
         return gameView;
     }
 
-    /**
-     * Loads players from a CSV file using FileService.
-     *
-     * @param file The CSV file to load players from
-     * @throws IOException If there is an error reading the file
-     */
-    public void loadPlayersFromCsv(File file) throws IOException {
-        logger.info("Loading players from CSV file: " + file.getAbsolutePath());
-
-        List<Player> loadedPlayers = fileService.loadPlayersFromFile(file);
-        gameView.logGameEvent("Loading " + loadedPlayers.size() + " players from " + file.getName());
-
-        // Add each player to the game if they don't already exist
-        int addedCount = 0;
-        for (Player player : loadedPlayers) {
-            if (boardGame.getPlayers().stream().noneMatch(p -> p.getName().equals(player.getName()))) {
-                boardGame.addPlayer(player);
-                gameView.addPlayer(player);
-                addedCount++;
-            }
-        }
-
-        gameView.logGameEvent("Added " + addedCount + " new players to the game.");
-    }
-
-    /**
-     * Saves players to a CSV file using FileService.
-     *
-     * @param file The CSV file to save players to
-     * @throws IOException If there is an error writing to the file
-     */
-    public void savePlayersToCsv(File file) throws IOException {
-        logger.info("Saving players to CSV file: " + file.getAbsolutePath());
-
-        fileService.savePlayersToFile(file, boardGame.getPlayers());
-        gameView.logGameEvent("Saved " + boardGame.getPlayers().size() + " players to " + file.getName());
-    }
-
-    /**
-     * Loads a board from a JSON file using FileService.
-     *
-     * @param file The JSON file to load the board from
-     * @throws JsonParsingException If there is an error parsing the JSON file
-     */
-    public void loadBoardFromFile(File file) throws JsonParsingException {
-        logger.info("Loading board from JSON file: " + file.getAbsolutePath());
-
-        try {
-            Board board = fileService.loadBoardFromFile(file);
-            boardGame.setBoard(board);
-
-            if (boardGame.getDice() == null) {
-                boardGame.createDice(2, 6);
-            }
-
-            this.gameView = new GameView(board);
-            boardGame.addObserver(gameView);
-
-            setupDiceDisplay();
-
-            gameView.setRollDiceAction(this::playTurn);
-
-            gameView.logGameEvent("Loaded board from " + file.getName());
-        } catch (JsonParsingException e) {
-            logger.log(Level.SEVERE, "Error loading board from file", e);
-            gameView.logGameEvent("Error loading board: " + e.getMessage());
-            throw e;
-        }
-    }
-
-    /**
-     * Saves the current board to a JSON file using FileService.
-     *
-     * @param file The JSON file to save the board to
-     * @throws IOException If there is an error writing to the file
-     * @throws JsonParsingException If there is an error formatting the JSON data
-     * @throws TileActionNotFoundException If a tile action cannot be found
-     */
-    public void saveBoardToFile(File file) throws IOException, JsonParsingException, TileActionNotFoundException {
-        logger.info("Saving board to JSON file: " + file.getAbsolutePath());
-
-        try {
-            fileService.saveBoardToFile(boardGame.getBoard(), file);
-            gameView.logGameEvent("Saved board to " + file.getName());
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error saving board to file", e);
-            gameView.logGameEvent("Error saving board: " + e.getMessage());
-            throw e;
-        }
-    }
-
-    /**
-     * Saves the current board to a JSON file with a custom name and description using FileService.
-     *
-     * @param file The JSON file to save the board to
-     * @param name The name of the board
-     * @param description The description of the board
-     * @throws IOException If there is an error writing to the file
-     * @throws JsonParsingException If there is an error formatting the JSON data
-     * @throws TileActionNotFoundException If a tile action cannot be found
-     */
-    public void saveBoardToFile(File file, String name, String description) throws IOException, JsonParsingException, TileActionNotFoundException {
-        logger.info("Saving board to JSON file: " + file.getAbsolutePath());
-
-        try {
-            fileService.saveBoardToPath(boardGame.getBoard(), file.getAbsolutePath(), name, description);
-            gameView.logGameEvent("Saved board to " + file.getName());
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error saving board to file", e);
-            gameView.logGameEvent("Error saving board: " + e.getMessage());
-            throw e;
-        }
-    }
-
-    /**
-     * Resets the game, clearing all players and setting game state to NOT_STARTED.
-     */
-    public void resetGame() {
-        logger.info("Resetting game");
-
-        boardGame.getPlayers().clear();
-        boardGame.startGame();
-
-        this.gameView = new GameView(boardGame.getBoard());
-        boardGame.addObserver(gameView);
-        setupDiceDisplay();
-        gameView.setRollDiceAction(this::playTurn);
-
-        gameView.logGameEvent("Game has been reset. Add players to start a new game.");
-    }
 
     /**
      * Abstract method to move a player a specific number of steps.
