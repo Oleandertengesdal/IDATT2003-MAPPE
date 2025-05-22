89f3b9370acc548e61966350d7747136e5d6de63
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-05-23 00:22:24 +0200

    Update FileService.java

--- src/main/java/idi/edu/idatt/mappe/controllers/FileService.java
+++ src/main/java/idi/edu/idatt/mappe/controllers/FileService.java
@@ -3,6 +3,7 @@ package idi.edu.idatt.mappe.controllers;
 import idi.edu.idatt.mappe.exceptions.JsonParsingException;
 import idi.edu.idatt.mappe.exceptions.TileActionNotFoundException;
 import idi.edu.idatt.mappe.models.Board;
+import idi.edu.idatt.mappe.models.GameRules;
 import idi.edu.idatt.mappe.models.Player;
 
 import java.io.File;
@@ -10,7 +11,12 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * Service for handling file operations related to players and game data.
+ * Service for handling file operations related to players, boards, and game rules.
+ * This interface provides methods for loading and saving players, boards, and game rules
+ * from/to files in various formats (CSV, JSON).
+ *
+ * @see Player, Board, GameRules
+ * @version 1.0
  */
 public interface FileService {
 
@@ -110,4 +116,60 @@ public interface FileService {
      * @throws IOException If an error occurs during file writing
      */
     void saveBoardToPath(Board board, String filePath, String name, String description) throws IOException, JsonParsingException, TileActionNotFoundException;
+
+    /**
+     * Loads game rules from a JSON file.
+     *
+     * @param file The file to load game rules from
+     * @return The loaded game rules
+     * @throws JsonParsingException If an error occurs during JSON parsing
+     */
+    GameRules loadGameRulesFromFile(File file) throws JsonParsingException;
+
+    /**
+     * Loads game rules from a specified file path.
+     *
+     * @param filePath The path to the file where the game rules are saved
+     * @return The loaded game rules
+     * @throws JsonParsingException If an error occurs during JSON parsing
+     */
+    GameRules loadGameRulesFromPath(String filePath) throws JsonParsingException;
+
+    /**
+     * Saves game rules to a JSON file.
+     *
+     * @param rules The game rules to save
+     * @param file The file to save the game rules to
+     * @throws IOException If an error occurs during file writing
+     * @throws JsonParsingException If an error occurs during JSON creation
+     */
+    void saveGameRulesToFile(GameRules rules, File file) throws IOException, JsonParsingException;
+
+    /**
+     * Saves game rules to a specified file path.
+     *
+     * @param rules The game rules to save
+     * @param filePath The path to the file where the game rules will be saved
+     * @throws IOException If an error occurs during file writing
+     * @throws JsonParsingException If an error occurs during JSON creation
+     */
+    void saveGameRulesToPath(GameRules rules, String filePath) throws IOException, JsonParsingException;
+
+    /**
+     * Saves game rules to a specified file path with a custom name.
+     *
+     * @param rules The game rules to save
+     * @param filePath The path to the file where the game rules will be saved
+     * @param name The name to use for the rules
+     * @throws IOException If an error occurs during file writing
+     * @throws JsonParsingException If an error occurs during JSON creation
+     */
+    void saveGameRulesToPath(GameRules rules, String filePath, String name) throws IOException, JsonParsingException;
+
+    /**
+     * Gets the default directory for game rules files.
+     *
+     * @return The default directory for game rules files
+     */
+    File getDefaultRulesDirectory();
 }
