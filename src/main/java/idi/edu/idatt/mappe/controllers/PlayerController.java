bd7e6708a838f7366d79ce7fb4246d089e0db5f4
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-05-23 00:22:31 +0200

    Update PlayerController.java

--- src/main/java/idi/edu/idatt/mappe/controllers/PlayerController.java
+++ src/main/java/idi/edu/idatt/mappe/controllers/PlayerController.java
@@ -68,7 +68,6 @@ public class PlayerController {
             return false;
         }
 
-        // Check if player already exists
         if (players.stream().anyMatch(p ->
                 p.getName().equalsIgnoreCase(player.getName()))) {
             logger.warning("Player with name " + player.getName() + " already exists");
@@ -78,100 +77,5 @@ public class PlayerController {
         players.add(player);
         logger.info("Added player: " + player.getName());
         return true;
-    }
-
-    /**
-     * Gets a player by name.
-     *
-     * @param name The name of the player to get
-     * @return The player with the given name, or null if not found
-     */
-    public Player getPlayerByName(String name) {
-        return players.stream()
-                .filter(p -> p.getName().equalsIgnoreCase(name))
-                .findFirst()
-                .orElse(null);
-    }
-
-    /**
-     * Removes a player from the collection.
-     *
-     * @param player The player to remove
-     * @return True if the player was removed successfully
-     */
-    public boolean removePlayer(Player player) {
-        boolean removed = players.remove(player);
-        if (removed) {
-            logger.info("Removed player: " + player.getName());
-        } else {
-            logger.warning("Failed to remove player: " + player.getName() + " (player not found)");
-        }
-        return removed;
-    }
-
-    /**
-     * Removes a player from the collection by name.
-     *
-     * @param playerName The name of the player to remove
-     * @return True if the player was removed successfully
-     */
-    public boolean removePlayer(String playerName) {
-        boolean removed = players.removeIf(player -> player.getName().equalsIgnoreCase(playerName));
-        if (removed) {
-            logger.info("Player removed: " + playerName);
-            return true;
-        }
-        logger.warning("Player not found: " + playerName);
-        return false;
-    }
-
-    /**
-     * Clears all players from the collection.
-     */
-    public void clearPlayers() {
-        players.clear();
-        logger.info("Cleared all players");
-    }
-
-    /**
-     * Loads players from a file using the FileService.
-     *
-     * @param file The file to load players from
-     * @throws IOException If an error occurs during file reading
-     */
-    public void loadPlayersFromFile(File file) throws IOException {
-        logger.info("Loading players from file: " + file.getAbsolutePath());
-
-        List<Player> loadedPlayers = fileService.loadPlayersFromFile(file);
-
-        // Update our collection
-        players.clear();
-        players.addAll(loadedPlayers);
-
-        logger.info("Loaded " + loadedPlayers.size() + " players from file");
-    }
-
-    /**
-     * Saves players to the default file.
-     *
-     * @throws IOException If an error occurs during file writing
-     */
-    public void savePlayersToDefaultFile() throws IOException {
-        File defaultFile = new File(fileService.getDefaultPlayerDirectory(), DEFAULT_PLAYER_FILENAME);
-        savePlayersToFile(defaultFile);
-    }
-
-    /**
-     * Saves players to a file using the FileService.
-     *
-     * @param file The file to save players to
-     * @throws IOException If an error occurs during file writing
-     */
-    public void savePlayersToFile(File file) throws IOException {
-        logger.info("Saving " + players.size() + " players to file: " + file.getAbsolutePath());
-
-        fileService.savePlayersToFile(file, new ArrayList<>(players));
-
-        logger.info("Players saved successfully");
     }
 }
