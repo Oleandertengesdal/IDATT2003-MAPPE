147c946d9af4ec34d7d284190386b8d367aa010b
Oleandertengesdal <oleander.tengesdal@icloud.com>
2025-05-23 00:24:59 +0200

    Create TileType.java

---
+++ src/main/java/idi/edu/idatt/mappe/models/enums/TileType.java
@@ -0,0 +1,15 @@
+package idi.edu.idatt.mappe.models.enums;
+
+/**
+ * Represents the type of tile on the board
+ * <p>
+ *     This is going to be used in The Lost Diamond, where we have different types of tiles.
+ *     We have, CITY, SEA_ROUTE, LAND_ROUTE, STARTING_CITY, FLIGHT_ROUTE.
+ */
+public enum TileType {
+    CITY,
+    SEA_ROUTE,
+    LAND_ROUTE,
+    STARTING_CITY,
+    FLIGHT_ROUTE
+}
